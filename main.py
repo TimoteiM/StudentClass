3a83d83300c9786be043dc185151213ca8cfabac
Moscaliuc Timotei <80130792+TimoteiM@users.noreply.github.com>
2022-05-02 19:33:21 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,61 +1,76 @@
-from STUDENT import Student
-
-class Database:
-    list=[]
-    def run(self):
-        student=Student(
-            input("Name: ").upper(),
-            input("Lastname: ").upper(),
-            float(input("Grades: ")),
-            input("Id: ")
-        )
-        self.list.append(student)
-        response=input("Do you want to enter another student? Type 'y' for yes")
-        if(response=='y'):
-            self.run()
-    def printStudents(self):
-        print("Name \t Lastname \t Grades \t Id")
-        for obj in self.list:
-            print(f"{obj.name} \t {obj.lastname} \t {obj.grade} \t {obj.id}")
-    def searchStudent(self,list,name):
-        for student in self.list:
-            if student.name==name:
-                print(f"{student.name} \t {student.lastname} \t {student.grade} \t {student.id}")
-    def modify_grade(self, name, lastname, grade):
-        for obj in self.list:
-            if(obj.name == name and obj.lastname == lastname):
-                print("Grade was modyfied succesfully!")
-                obj.grade=grade
-    def sortByName(self,list):
-        for i in self.list:
-            for j in self.list:
-                if i.name>j.name:
-                    aux = i
-                    i = j
-                    j = aux
-
-
-db = Database()
-def Menu():
-    print("1. Enter Students.")
-    print("2. Print Students.")
-    print("3. Search Student by name.")
-    print("4. Modify grade by name.")
-    print("5. Sort list by name.")
-    print("0. Exit Menu.")
-option=1
-Menu()
-while option != 0:
-    option = int(input("Choose an option: "))
-    if option == 1:
-        db.run()
-    elif option == 2:
-        db.printStudents()
-    elif option == 3:
-        db.searchStudent(list, input("Name: ").upper())
-    elif option == 4:
-        db.modify_grade(name=input("Enter name: ").upper(), lastname=input("Enter lastname: ").upper(), grade=float(input("Enter the new grade: ")))
-    elif option == 5:
-        db.sortByName(list)
+import pandas
+def add_student():
+    students_number = int(input("How many students do you want to add?"))
+    if students_number == 0:
+        pass
     else:
-        exit(0)
+        with open("Students.csv", mode="a") as file:
+            for x in range(0,students_number):
+                file.write("\n")
+                id = int(input("Write the student's id: "))
+                name = input("Write full name: ")
+                grades = input("Write the student's grades: ")
+                file.writelines(f"{id},{name},{grades}")
+
+def menu():
+    print("1.Incarcare informatii despre studenti\n"
+          "2.Afisare Studenti\n"
+          "3.Afisare note\n"
+          "4.Afisare studenti si notele acestora\n"
+          "5.Cautare student dupa nume\n"
+          "6.Afisare studenti promovati\n"
+          "7.Info autor\n"
+          "8.Exit\n")
+
+def Average(lista):
+    average = sum(lista)/len(lista)
+    return average
+
+data = pandas.read_csv("Students.csv")
+
+menu()
+
+
+option = 1
+while option != 0:
+    option = int(input("Introdu o optiune:"))
+    if option == 1:
+        add_student()
+
+    elif option == 2:
+        for (index, row) in data.iterrows():
+            print(f"{row.id}\t{row.nume}")
+
+    elif option == 3:
+        print(data.to_dict(orient='series')['note'])
+
+    elif option == 4:
+        print("id\tnume\tnote")
+        for (index, row) in data.iterrows():
+            print(f"{row.id}\t{row.nume}\t{row.note}")
+
+    elif option == 5:
+        search_by_name = input("Introdu numele cautat:")
+        lista_nume = []
+        for (index,row) in data.iterrows():
+            lista_nume.append(row.nume)
+        if search_by_name in lista_nume:
+            print(search_by_name)
+            print("Studentul a fost gasit")
+        else:
+            print("Studentul nu se afla in baza de date")
+
+    elif option == 6:
+        lista_note = []
+        print("Studentii promovati:\n")
+        for (index,row) in data.iterrows():
+            lista_note = row.note.split()
+            lista_note = [int(i) for i in lista_note]
+            if Average(lista_note) >= 5.00:
+                print(f"{row.id}\t{row.nume}\t{round(Average(lista_note),2)}")
+    elif option == 7:
+        print("Program realizat de Ing. Timotei Moscaliuc, student anul I - Calculatoare")
+    else:
+        option = 0
+
+
